4f908f2a4f6cde5446a932aba2a71c6f22f0218c
Amaliebra <jba082024@gmail.com>
2024-11-27 14:20:16 +0100

    added folders and index

--- DataAggregator/Program.cs
+++ DataAggregator/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+
+
+
+app.MapControllers();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
