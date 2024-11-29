9263aea019a8e749b1901adf11b110f4be815ee9
Amaliebra <amaliebratland@gmail.com>
2024-11-29 10:40:37 +0100

    razor had issues with displaying html, swtching back

--- DataAggregator/Controller/ScrapingController.cs
+++ DataAggregator/Controller/ScrapingController.cs
@@ -8,17 +8,15 @@ namespace DataAggregator.Controllers
         [HttpGet("/")]
         public async Task<IActionResult> Index()
         {
-            string url = "https://www.scrapingcourse.com/ecommerce/";
+            string url = "https://www.scrapethissite.com/pages/forms/";
 
             var web = new HtmlWeb();
             var document = await web.LoadFromWebAsync(url);
 
             string FullHtml = document.DocumentNode.OuterHtml;
-            string IndexPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html");
-            string HtmlToIndex = await System.IO.File.ReadAllTextAsync(IndexPath);
-            string UpdateIndex = HtmlToIndex.Replace("{{SCRAPED_HTML}}", FullHtml);
+            string EncodedHtml = System.Web.HttpUtility.HtmlEncode(FullHtml);
 
-            return Content(UpdateIndex, "text/html");
+            return View(EncodedHtml);
         }
     }
 }
